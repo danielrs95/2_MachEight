b90b1ce31514bbb1b97ed9b5a2840bd97b62e8dc
Daniel <danielrs9504@gmail.com>
2022-02-14 18:36:25 -0500

    Fetch data with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [response, setResponse] = useState();
+
+  useEffect(() => {
+    const axiosReq = async () => {
+      const res = await axios.get('https://mach-eight.uc.r.appspot.com/');
+      // console.log(res.data.values);
+      setResponse(res.data.values);
+    };
+
+    axiosReq();
+  }, []);
+
+  console.log(response);
+
   return (
     <div className='App'>
       <header className='App-header'>
+        {response[0].first_name}
         <p>Hello World</p>
       </header>
     </div>
